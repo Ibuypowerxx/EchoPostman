edddc231bb194ed523138401027d580ae7afb7be
ibuypower <danroz1995@yandex.ru>
2024-03-19 19:45:32 +0300

    InitialCommit

---
+++ src/test/java/ru/netology/Test.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class TestApi {
+    @Test
+    void shouldReturnAcc() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("HelloWorld") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data",org.hamcrest.Matchers.equalTo("HelloWorld"))
+        ;
+    }
+}
